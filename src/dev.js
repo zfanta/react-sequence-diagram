fca194a0a25d029fd7d7ae7dbb0c628a572c6282
Hwantae Ji <jht008@gmail.com>
2017-09-05 12:30:00 +0900

    Input diagram string

--- src/dev.js
+++ src/dev.js
@@ -3,4 +3,10 @@ import { render } from 'react-dom';
 
 import SequenceDiagram from './index';
 
-render(<SequenceDiagram />, document.getElementById('root'));
+const input =
+  "Andrew->China: Says Hello\n" +
+  "Note right of China: China thinks\nabout it\n" +
+  "China-->Andrew: How are you?\n" +
+  "Andrew->>China: I am good thanks!\n";
+
+render(<SequenceDiagram input={input} />, document.getElementById('root'));
--- src/index.js
+++ src/index.js
@@ -1,8 +1,13 @@
 import React, { Component } from 'react';
 
 class SequenceDiagram extends Component {
+  componentDidMount() {
+    const { input } = this.props;
+    console.log(input);
+  }
+
   render() {
-    return <div>test</div>
+    return <div ref={div => { this.div = div }} />
   }
 }
 
