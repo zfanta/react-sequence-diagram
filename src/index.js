6bc4b5f4d72a558ffdb98f83b949b7745ace0a44
Hwantae Ji <jht008@gmail.com>
2017-09-05 13:40:58 +0900

    Render sequence diagram

--- src/index.js
+++ src/index.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 
+import Diagram from './sequence-diagram-2.0.1';
+
 class SequenceDiagram extends Component {
   componentDidMount() {
-    const { input } = this.props;
-    console.log(input);
+    const { input, options } = this.props;
+    const diagram = Diagram.parse(input);
+    if (this.div) {
+      diagram.drawSVG(this.div, options)
+    }
   }
 
   render() {
