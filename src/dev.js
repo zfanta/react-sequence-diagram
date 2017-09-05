d55b6d166fcdc8fc25968c14c66bd566dc5fc49f
Hwantae Ji <jht008@gmail.com>
2017-09-05 13:41:32 +0900

    Modify test input string
    " -> '
    \n => \\n

--- src/dev.js
+++ src/dev.js
@@ -4,9 +4,10 @@ import { render } from 'react-dom';
 import SequenceDiagram from './index';
 
 const input =
-  "Andrew->China: Says Hello\n" +
-  "Note right of China: China thinks\nabout it\n" +
-  "China-->Andrew: How are you?\n" +
-  "Andrew->>China: I am good thanks!\n";
+  'Andrew->China: Says Hello\n' +
+  'Note right of China: China thinks\\nabout it\n' +
+  'China-->Andrew: How are you?\n' +
+  'Andrew->>China: I am good thanks!';
+
 
 render(<SequenceDiagram input={input} />, document.getElementById('root'));
